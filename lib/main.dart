efb7c885c00b848d98e150126ba6d41e018cfa26
HosseinAsadi <haviking76@gmail.com>
2022-07-25 11:41:08 +0430

    app theme file was created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:digit41/utils/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/screens/splash.dart';
@@ -13,10 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Digit41',
-      theme: ThemeData(
-        primaryColor: Colors.black,
-        scaffoldBackgroundColor: Colors.black,
-      ),
+      theme: AppTheme.theme,
       home: const Splash(),
     );
   }
---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
+    primaryColor: const Color(0xffA8F358),
+  );
+}

fdde7711d00de5706ffedd656987b92ec3348881
HosseinAsadi <haviking76@gmail.com>
2022-08-02 15:30:33 +0430

    show insufficient balance in unlock post

--- lib/presentation/screens/post/unlock_post.dart
+++ lib/presentation/screens/post/unlock_post.dart
@@ -1,9 +1,9 @@
-import 'package:digit41/presentation/global_widgets/app_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../utils/images_path.dart';
 import '../../../utils/strings.dart';
+import '../../global_widgets/app_button.dart';
 import 'widgets/post_media.dart';
 import 'widgets/post_top_info.dart';
 
@@ -14,7 +14,7 @@ class UnlockPost extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const PostMedia(height: 220.0),
+        const PostMedia(height: 210.0),
         const SizedBox(height: 8.0),
         const PostTopInfo(
           nameTrailing: Text(
@@ -26,6 +26,7 @@ class UnlockPost extends StatelessWidget {
         _info(Strings.price, '3'),
         const SizedBox(height: 40.0),
         _info(Strings.totalWalletBalance, '234'),
+        Visibility(visible: true, child: _notEnoughBalance()),
         const SizedBox(height: 32.0),
         AppButton(title: Strings.unlockItem, onTap: () {}),
       ],
@@ -54,5 +55,36 @@ class UnlockPost extends StatelessWidget {
             ],
           ),
         ],
+      );
+
+  Widget _notEnoughBalance() => Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(25.0),
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 7.0),
+              child: const Text(
+                'askdasdksdksadkas',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            Positioned(
+              left: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child: const Icon(Icons.clear, color: Colors.white, size: 14.0),
+              ),
+            ),
+          ],
+        ),
       );
 }
